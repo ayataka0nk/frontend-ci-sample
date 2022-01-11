89b9a21fa0fa2f6afdb42362e6241a8285370262
Naoto Kamba <naoto.kamba@outlook.jp>
2022-01-11 12:47:10 +0900

    二つ以上のビルドを同時にするサンプル

---
+++ view2/src/App.tsx
@@ -0,0 +1,17 @@
+import React from 'react';
+import './App.css';
+
+function App() {
+  console.log(process.env.REACT_APP_BUILD_ENV)
+  return (
+    <div className="App">
+        <div>Environmental Value Sample View2</div>
+        <div>
+          <div>REACT_APP_BUILD_ENV: </div>
+          <div>{process.env.REACT_APP_BUILD_ENV}</div>
+        </div>
+      </div>
+  );
+}
+
+export default App;
